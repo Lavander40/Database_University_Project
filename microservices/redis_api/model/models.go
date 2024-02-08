1ba6f4d1920b75c81784a63cae1e8d3c81d86801
Lavander40 <a.kirill.03@mail.ru>
2024-02-09 02:01:45 +0300

    readme edits

--- microservices/redis_api/model/models.go
+++ microservices/redis_api/model/models.go
@@ -2,6 +2,6 @@ package model
 
 type Student struct {
 	Id      string `json:"id"`
-	Name    string `json:"full_name"`
-	GroupId string `json:"group_id"`
+	Name    string `json:"name"`
+	GroupId string `json:"group"`
 }
